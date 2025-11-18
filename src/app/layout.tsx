1c5dbd992a50da73aa8e506bb6fa6512918a693b
K1RShiN <shinasuka313819@gmail.com>
2025-11-18 10:55:07 +0900

    Speed Insights機能3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Analytics } from "@vercel/analytics/react";
 
-import { SpeedInsights } from "@vercel/speed-insights";
+import SpeedInsights from "@vercel/speed-insights/next";
 
 const geistSans = GeistSans;
 
