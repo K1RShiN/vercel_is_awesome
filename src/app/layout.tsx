54bef4f4d736bb2cbbef86e7411efd2b66299da2
K1RShiN <shinasuka313819@gmail.com>
2025-11-18 09:50:11 +0900

    アナリティクス機能を追加（？）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${Geist_Mono.variable} antialiased`}
+      >
+        {children}
+        <Analytics /> {/* この行を追加 */}
+      </body>
     </html>
   );
 }
